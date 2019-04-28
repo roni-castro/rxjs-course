b78a941748f4a5cf29b13fc803adf099dd0d4f00
Roni <roni.c.castro@gmail.com>
2019-04-28 19:18:14 -0300

    Add begginer and advanced course to home screen

--- src/app/common/util.ts
+++ src/app/common/util.ts
@@ -1,2 +1,18 @@
+import { Observable } from 'rxjs';
 
+export function createHttpObservable(url) {
+  return Observable.create(observer => {
+    fetch(url)
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      observer.next(data);
+      observer.complete();
+    })
+    .catch(err => {
+      observer.error(err);
+    });
+  });
+}
 
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Course} from "../model/course";
 import {interval, Observable, of, timer} from 'rxjs';
 import {catchError, delayWhen, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
+import { createHttpObservable } from '../common/util';
 
 
 @Component({
@@ -10,16 +11,33 @@ import {catchError, delayWhen, map, retryWhen, shareReplay, tap} from 'rxjs/oper
     styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+    private beginnerCourses: Course[];
+    private advancedCourses: Course[];
 
     constructor() {
-
     }
 
     ngOnInit() {
 
+      const http$ = createHttpObservable('/api/courses');
+      const courses$ = http$
+        .pipe(map(res => {
+          return Object.values(res['payload']);
+        }));
+
+        courses$.subscribe(courses => {
+          this.beginnerCourses = courses.filter(course => course.category === 'BEGINNER');
+          this.advancedCourses = courses.filter(course => course.category === 'ADVANCED');
+          console.log(courses);
+        }, err => {
+          console.log('Error: ' + err);
+        },
+          console.log('completed')
+        );
+      }
 
 
     }
+
 
 }
